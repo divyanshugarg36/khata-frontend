a1693325ad525b8bb94ce3e858ef722b32e3a000
Divyanshu Garg <divyanshugarg36@gmail.com>
2020-01-12 00:12:17 +0530

    added a form

---
+++ src/components/login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <form>
+        <input />
+        <input />
+        <button />
+      </form>
+    );
+  }
+}
+
+export default Login;
