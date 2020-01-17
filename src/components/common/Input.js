ff115d805d1ac3887967a98fbd0899e311954589
Divyanshu Garg <divyanshugarg36@gmail.com>
2020-01-17 09:01:14 +0530

    input updated

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 
 const Input = React.forwardRef((props, ref) => {
   const { label, id, type } = props;
+  const newId = !id ? Math.random().toString(36).substring(2) : id;
   return (
     <div className="input-container">
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={newId}>{label}</label>
       <input
         ref={ref}
-        id={id}
+        id={newId}
         type={type}
       />
     </div>
@@ -16,12 +17,13 @@ const Input = React.forwardRef((props, ref) => {
 });
 
 Input.propTypes = {
-  id: PropTypes.string.isRequired,
+  id: PropTypes.string,
   label: PropTypes.string.isRequired,
   type: PropTypes.string,
 };
 
 Input.defaultProps = {
+  id: '',
   type: 'text',
 };
 
