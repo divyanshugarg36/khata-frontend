bd773d47c74cc748506627d31e9c106939572885
Satnam Singh <satnam9781@gmail.com>
2020-03-04 23:39:02 +0530

    Add page to edit details of member

---
+++ src/components/member/editMember.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import Input from '../common/Input';
+import { API } from '../../api';
+
+class Member extends Component {
+  constructor(props) {
+    super(props);
+    const { match: { params: { id } } } = props;
+    this.state = {
+      id,
+      member: null,
+      history: props.history,
+    };
+
+    this.username = React.createRef();
+    this.name = React.createRef();
+    this.email = React.createRef();
+  }
+
+  componentDidMount() {
+    const { id } = this.state;
+    axios.post(API.fetchUser, { id })
+      .then(({ data }) => {
+        this.setState({ member: data.user });
+      })
+      .catch((err) => console.log(err.response));
+  }
+
+  removeMember = () => {
+    const { id, history: { push } } = this.state;
+    axios.post(API.deleteMember, { id })
+      .then(() => {
+        push('/member/all');
+        window.alert('Member removed!');
+      })
+      .catch((err) => console.log(err));
+  }
+
+  update = () => {
+    const { id } = this.state;
+    const {
+      username: { current: { value: username } },
+      name: { current: { value: name } },
+      email: { current: { value: email } },
+    } = this;
+    const data = {
+      id,
+      username,
+      name,
+      email,
+    };
+    axios.put(API.update, data)
+      .then(() => {
+        window.alert('Member details updated!');
+      })
+      .catch((err) => {
+        window.alert(err.response.data.info || 'Member details not updated!');
+      });
+  }
+
+  render() {
+    const { removeMember, update } = this;
+    const { member } = this.state;
+    return (
+      <>
+        {member
+          && (
+            <div>
+              <button onClick={() => { removeMember(); }}>Remove Member</button>
+              <h3>Edit member details - </h3>
+              <Input
+                label="Name"
+                ref={this.name}
+                value={member.name}
+              />
+              <Input
+                label="Username"
+                ref={this.username}
+                value={member.username}
+              />
+              <Input
+                label="Email"
+                ref={this.email}
+                type="email"
+                value={member.email}
+              />
+              <button onClick={update}>Update</button>
+            </div>
+          )}
+      </>
+    );
+  }
+}
+
+Member.propTypes = {
+  match: PropTypes.instanceOf(Object).isRequired,
+  history: PropTypes.instanceOf(Object).isRequired,
+};
+
+Member.defaultProps = {};
+
+export default withRouter(Member);
