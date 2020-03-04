ab44a42e9b2358947605bf9183e9b45f23f86ea9
Satnam Singh <satnam9781@gmail.com>
2020-03-05 00:15:44 +0530

    Remove editing part from project view page

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import Input from '../common/Input';
-import Select from '../common/Select';
 import { API } from '../../api';
 
 class Project extends Component {
@@ -15,11 +13,6 @@ class Project extends Component {
       project: null,
       history: props.history,
     };
-
-    this.user = React.createRef();
-    this.role = React.createRef();
-    this.price = React.createRef();
-    this.type = React.createRef();
   }
 
   componentDidMount() {
@@ -29,46 +22,6 @@ class Project extends Component {
         this.setState({ project: data.project });
       })
       .catch((err) => console.log(err.response));
-  }
-
-  addMember = () => {
-    const {
-      user: { current: { value: user } },
-      role: { current: { value: role } },
-      price: { current: { value: price } },
-      type: { current: { value: type } },
-    } = this;
-    const { id } = this.state;
-    const data = {
-      username: user,
-      project: id,
-      role,
-      price,
-      type,
-    };
-    axios.post(API.assignMember, data)
-      .then(({ data }) => {
-        this.setState({ project: data.project });
-        window.alert('User added!');
-      })
-      .catch((err) => window.alert(err.response.data.info || 'User not added!'));
-  }
-
-  removeMember = (userId) => {
-    const { id: project } = this.state;
-    axios.post(API.removeMember, { userId, project })
-      .then(() => {
-        const { project } = this.state;
-        const { assignments } = project;
-        assignments.forEach((item, index) => {
-          if (item.id === userId) {
-            assignments.splice(index, 1);
-          }
-        });
-        this.setState({ project });
-        window.alert('User removed!');
-      })
-      .catch((err) => console.log(err));
   }
 
   removeProject = () => {
@@ -83,8 +36,7 @@ class Project extends Component {
 
   render() {
     const { id, project, history: { push } } = this.state;
-    const { removeProject, addMember, removeMember } = this;
-    const types = ['Hourly', 'Monthly', 'Other'];
+    const { removeProject } = this;
     return (
       <>
         {project
@@ -92,8 +44,6 @@ class Project extends Component {
             <div>
               <div className="details-container">
                 <h3>Project Details - </h3>
-                <button onClick={() => { push(`/project/${id}/invoice`); }}>Generate Invoice</button>
-                <button onClick={() => removeProject()}>Remove</button>
                 <br />
                 <strong> Name - </strong>
                 {project.name}
@@ -113,34 +63,15 @@ class Project extends Component {
                     ({ id, name, username }) => (
                       <li key={id}>
                         { name || username }
-                        <button onClick={() => removeMember(id)}>Remove</button>
                       </li>
                     ),
                   )}
                 </ul>
               </div>
-              <div className="form-container">
-                <h3>Add member to project - </h3>
-                <Input
-                  label="Username"
-                  ref={this.user}
-                />
-                <Input
-                  label="Role"
-                  ref={this.role}
-                />
-                <Input
-                  label="Price"
-                  ref={this.price}
-                  type="number"
-                />
-                <Select
-                  label="Username"
-                  ref={this.type}
-                  options={types}
-                />
-                <button onClick={addMember}>Add member</button>
-              </div>
+              <button onClick={() => { push(`/project/edit/${id}`); }}>Edit Project</button>
+              <button onClick={() => { push(`/project/${id}/invoice`); }}>Generate Invoice</button>
+              <button onClick={() => removeProject()}>Remove</button>
+
             </div>
           )}
       </>
