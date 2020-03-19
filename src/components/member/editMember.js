5352bb6cdd4ab68719984744153999df02cf25e8
Satnam Singh <satnam9781@gmail.com>
2020-03-20 01:33:36 +0530

    Bug fix & code simplification

--- src/components/member/editMember.js
+++ src/components/member/editMember.js
@@ -18,7 +18,11 @@ class EditMember extends Component {
   }
 
   componentDidMount() {
-    getEntity(API.user.get, { id: this.id }, ({ user: member }) => this.setState({ member }));
+    getEntity(
+      API.user.get,
+      { id: this.id },
+      ({ user: member }) => this.setState({ member }),
+    );
   }
 
   removeMember = () => {
@@ -26,8 +30,7 @@ class EditMember extends Component {
   }
 
   update = (data) => {
-    const { id } = this.state;
-    data.id = id;
+    data.id = this.id;
     axios.put(API.user.update.profile, data)
       .then(() => {
         window.alert('Member details updated!');
