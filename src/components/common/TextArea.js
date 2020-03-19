09fdfc0bb93fd60cbfca49712935e701f4885c97
Satnam Singh <satnam9781@gmail.com>
2020-03-19 20:31:45 +0530

    Add new textarea component

---
+++ src/components/common/TextArea.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TextArea = React.forwardRef((props, ref) => {
+  const {
+    label,
+    id,
+    value,
+  } = props;
+  const newId = !id ? Math.random().toString(36).substring(2) : id;
+  return (
+    <div className="textarea-container">
+      <label htmlFor={newId}>{label}</label>
+      <br />
+      <input
+        ref={ref}
+        id={newId}
+        defaultValue={value}
+      />
+    </div>
+  );
+});
+
+TextArea.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string,
+};
+
+TextArea.defaultProps = {
+  id: '',
+  value: '',
+};
+
+export default TextArea;
