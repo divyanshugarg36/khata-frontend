c1eecf55176b6d47cf87a209e23ab6b7cacdea64
Satnam Singh <satnam9781@gmail.com>
2020-03-20 23:18:23 +0530

    Remove duplication in invoices list page

--- src/components/invoice/invoiceList.js
+++ src/components/invoice/invoiceList.js
@@ -15,7 +15,7 @@ class InvoiceList extends Component {
   }
 
   componentDidMount() {
-    getEntity(API.invoice.all, '{}', (data) => this.setState({ invoices: data.invoices }));
+    getEntity(API.invoice.all, '', ({ invoices }) => this.setState({ invoices }));
   }
 
   render() {
